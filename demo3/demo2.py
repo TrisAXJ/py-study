e10b3ee287b8ab1035d37ccb3092ac14b42e8f3b
trisanima <1628791325@qq.com>
2021-01-22 17:47:54 +0800

    文件打开

---
+++ demo3/demo2.py
@@ -0,0 +1,11 @@
+# -*- codeing = utf-8 -*-
+# @Author:TRISA QQ:1628791325 trisanima.cn
+# @Time : 2021/1/22 17:43
+# @File : demo2.py
+# @Software: PyCharm
+
+
+f = open("text.txt","w")    # 打开文件，w模式（写模式），文件不存在就新建
+
+f.close()       # 关闭文件
+
