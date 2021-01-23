2a196ab65852a1bcc7ddc5f6073953dad9c81f76
trisanima <1628791325@qq.com>
2021-01-23 18:57:31 +0800

    完善main

--- douban/spider.py
+++ douban/spider.py
@@ -18,13 +18,48 @@ def main():
     datalist = getData(baseurl)
     savepath = ".\\豆瓣电影Top250.xls"
     # 3.保存数据
-    saveData(savepath)
+    #saveData(savepath)
+
+    askURL("https://movie.douban.com/top250?start=")
+
 
 # 爬取网页
 def getData(baseurl):
     datalist = []
-    # 2.逐一解析数据
+    for i in range(0,10):        # 调用获取页面信息的函数，10次
+        url = baseurl + str(i*25)
+        html = askURL(url)      # 保存获取到的网页源码
+
+        # 2.逐一解析数据
+
+
+
     return datalist
+
+
+# 得到指定一个URL的网页内容
+def askURL(url):
+    head = {            # 模拟浏览器头部信息，向豆瓣服务器发送信息
+        "User-Agent": "Mozilla / 5.0(Windows NT 10.0;Win64;x64) AppleWebKit / 537.36(KHTML, likeGecko) Chrome / 87.0.4280.141Safari / 537.36"
+    }
+                        # 用户代理，表示告诉豆瓣服务器，我们是什么类型的机器、浏览器（本质上是告诉服务器我们可以接受什么水平的文件内容）
+    request = urllib.request.Request(url,headers=head)
+    html = ""
+    try:
+        response = urllib.request.urlopen(request)
+        html = response.read().decode("utf-8")
+        print(html)
+    except urllib.error.URLError as e:
+        if hasattr(e,"code"):
+            print(e.code)
+        if hasattr(e,"reason"):
+            print(e.reason)
+
+    return html
+
+
+
+
 
 # 3.保存数据
 def saveData(savepath):
